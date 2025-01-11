173e4ac6687aa2f1f56b7219a415b50f1a842456
zuojian lin <manjian2006@gmail.com>
2025-01-11 16:56:54 +0800

    Fix time stamp showing

--- script.js
+++ script.js
@@ -925,10 +925,13 @@ class VideoProcessor {
 
     // Convert milliseconds to hh:mm:ss
     const date = new Date(totalTime);
+    const year = date.getUTCFullYear();
+    const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(date.getUTCDate()).padStart(2, "0");
     const hours = String(date.getUTCHours()).padStart(2, "0");
     const minutes = String(date.getUTCMinutes()).padStart(2, "0");
     const seconds = String(date.getUTCSeconds()).padStart(2, "0");
-    const text = `Time: ${hours}:${minutes}:${seconds}`;
+    const text = `Time: ${year}-${month}-${day} ${hours}-${minutes}-${seconds}`;
 
     this.ctx.fillStyle = "white";
     this.ctx.font = "20px Arial";
